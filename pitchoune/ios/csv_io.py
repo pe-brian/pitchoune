2ee9cfa31cd2d0c927f85bdd6d8870ad8c0cbfd3
Pierre-Emmanuel BRIAN <pebrian@outlook.fr>
2025-05-22 13:57:10 +0200

    csv_io params

--- pitchoune/ios/csv_io.py
+++ pitchoune/ios/csv_io.py
@@ -10,10 +10,10 @@ class CSV_IO(IO):
     def __init__(self):
         super().__init__(suffix="csv")
 
-    def deserialize(self, filepath: Path|str, schema=None, separator: str=";", decimal_comma: bool = False) -> None:
+    def deserialize(self, filepath: Path|str, schema=None, separator: str=";", decimal_comma: bool = False, **params) -> None:
         """Read a CSV file and return a Polars DataFrame."""
-        return pl.read_csv(str(filepath), schema_overrides=schema, encoding="utf-8", separator=separator, decimal_comma=decimal_comma)
+        return pl.read_csv(str(filepath), schema_overrides=schema, encoding="utf-8", separator=separator, decimal_comma=decimal_comma, **params)
 
-    def serialize(self, df: pl.DataFrame, filepath: Path|str, separator: str=";") -> None:
+    def serialize(self, df: pl.DataFrame, filepath: Path|str, separator: str=";", **params) -> None:
         """Write a Polars DataFrame to a CSV file."""
-        df.write_csv(str(filepath), separator=separator, quote_style="non_numeric", include_bom=True)
+        df.write_csv(str(filepath), separator=separator, quote_style="non_numeric", include_bom=True, **params)
--- pitchoune/ios/tsv_io.py
+++ pitchoune/ios/tsv_io.py
@@ -10,10 +10,10 @@ class TSV_IO(IO):
     def __init__(self):
         super().__init__(suffix="tsv")
 
-    def deserialize(self, filepath: Path|str, schema=None, separator: str="\t", decimal_comma: bool = False) -> None:
+    def deserialize(self, filepath: Path|str, schema=None, decimal_comma: bool = False, **params) -> None:
         """Read a TSV file and return a Polars DataFrame."""
-        return pl.read_csv(str(filepath), schema_overrides=schema, encoding="utf-8", separator=separator, decimal_comma=decimal_comma)
+        return pl.read_csv(str(filepath), schema_overrides=schema, encoding="utf-8", separator="\t", decimal_comma=decimal_comma, **params)
 
-    def serialize(self, df: pl.DataFrame, filepath: Path|str, separator: str="\t") -> None:
+    def serialize(self, df: pl.DataFrame, filepath: Path|str, **params) -> None:
         """Write a Polars DataFrame to a TSV file."""
-        df.write_csv(str(filepath), separator=separator, quote_style="non_numeric", include_bom=True)
+        df.write_csv(str(filepath), separator="\t", quote_style="non_numeric", include_bom=True, **params)
